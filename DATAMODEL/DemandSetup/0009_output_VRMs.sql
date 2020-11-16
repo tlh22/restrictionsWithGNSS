1a6318ddd3cfc3a988b2893fc297afbb1b8c4099
tlh22 <thancock@mhtc.co.uk>
2020-11-16 10:54:54 +0100

    ... minor fixes

--- DATAMODEL/DemandSetup/0009_output_VRMs.sql
+++ DATAMODEL/DemandSetup/0009_output_VRMs.sql
@@ -2,7 +2,7 @@
 
 SELECT "ID", v."SurveyID" AS "SurveyID", "SurveyDay", "BeatTitle" AS "SurveyTimePeriod", "SectionID", "GeometryID", "PositionID",
        "VRM", "VehicleTypeID", "VehicleTypes"."Description" AS "VehicleType Description", "RestrictionTypeID", "BayLineTypes"."Description" AS "RestrictionType Description", "Notes"
-FROM ((("demand"."VRMs_Wednesday_TheRookery" v
+FROM ((("demand"."VRMs_Saturday_Zone_J" v
      LEFT JOIN "demand"."Surveys" AS "Surveys" ON v."SurveyID" = "Surveys"."SurveyID")
      LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON v."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
      LEFT JOIN "demand_lookups"."VehicleTypes" AS "VehicleTypes" ON v."VehicleTypeID" is not distinct from "VehicleTypes"."Code")
