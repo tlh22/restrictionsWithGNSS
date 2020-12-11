240ecd50a04206bd84577c47bf2446f5de13b5c5
tlh22 <thancock@mhtc.co.uk>
2020-12-11 18:34:39 +0100

    Create enfield_output.sql

---
+++ Database Scripts/enfield_output.sql
@@ -0,0 +1,12 @@
+SELECT "Area", "GeometryID", "RoadName", "Location", "RestrictionLength", "NrBays", "Notes", "LastUpdateDateTime", "LastUpdatePerson", "MHTC_CheckIssueTypeID", "MHTC_CheckNotes", "Photos_01", name, sections, bay_type, baylist_type, "TimePeriod_Description", "MaxStay_Description", "TheoreticalSpaces", "Capacity", baylist_notes_1, baylist_notes_2,
+"BayLineTypes"."Description" AS "RestrictionDescription",
+COALESCE("TimePeriods1"."Description", '') AS "Details of Control",
+COALESCE("LengthOfTime"."Description", '') AS "Max Stay",
+COALESCE("RestrictionGeomShapeTypes"."Description", '') AS "Restriction Shape Description"
+	FROM ((((local_authority.enfield_bays a
+
+     LEFT JOIN "toms_lookups"."BayLineTypes" AS "BayLineTypes" ON a."RestrictionTypeID" is not distinct from "BayLineTypes"."Code")
+     LEFT JOIN "toms_lookups"."RestrictionGeomShapeTypes" AS "RestrictionGeomShapeTypes" ON a."GeomShapeID" is not distinct from "RestrictionGeomShapeTypes"."Code")
+     LEFT JOIN "toms_lookups"."TimePeriods" AS "TimePeriods1" ON a."TimePeriodID" is not distinct from "TimePeriods1"."Code")
+	 LEFT JOIN "toms_lookups"."LengthOfTime" AS "LengthOfTime" ON a."MaxStayID" is not distinct from "LengthOfTime"."Code")
+	ORDER BY "Area", "GeometryID"
