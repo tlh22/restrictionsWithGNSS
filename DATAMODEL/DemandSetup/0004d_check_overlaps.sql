de547bb92dcbcabdc05d6f27651bbeb769e34dda
tlh22 <thancock@mhtc.co.uk>
2020-12-16 07:35:52 +0100

    Update 0004d_check_overlaps.sql

--- DATAMODEL/DemandSetup/0004d_check_overlaps.sql
+++ DATAMODEL/DemandSetup/0004d_check_overlaps.sql
@@ -8,4 +8,11 @@ FROM mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
 WHERE ST_Overlaps(s1.geom, s2.geom)
 ORDER BY s1."RoadName";
 
+ALTER TABLE ONLY mhtc_operations."Supply_Overlaps"
+    ADD CONSTRAINT "Supply_Overlaps_pkey" PRIMARY KEY ("GeometryID");
+
+ALTER TABLE mhtc_operations."Supply_Overlaps"
+  OWNER TO postgres;
+GRANT ALL ON TABLE mhtc_operations."Supply_Overlaps" TO postgres;
+
 
