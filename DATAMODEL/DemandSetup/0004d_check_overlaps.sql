10d04bb3ebad45efe12bdb78c2f7ef1e18f86f49
tlh22 <thancock@mhtc.co.uk>
2021-03-04 14:43:21 +0100

    Update 0004d_check_overlaps.sql

--- DATAMODEL/DemandSetup/0004d_check_overlaps.sql
+++ DATAMODEL/DemandSetup/0004d_check_overlaps.sql
@@ -2,14 +2,19 @@
 
 DROP TABLE IF EXISTS mhtc_operations."Supply_Overlaps";
 
-CREATE TABLE mhtc_operations."Supply_Overlaps" AS
+CREATE TABLE mhtc_operations."Supply_Overlaps"
+(
+    gid SERIAL,
+	"GeometryID" character varying(12) COLLATE pg_catalog."default" NOT NULL,
+	"RoadName" character varying(254) COLLATE pg_catalog."default",
+    geom geometry(Geometry,27700)
+);
+
+INSERT INTO mhtc_operations."Supply_Overlaps" ("GeometryID", "RoadName", geom)
 SELECT s1."GeometryID", s1."RoadName", ST_Intersection(s1.geom, s2.geom) AS geom
 FROM mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
 WHERE ST_Overlaps(s1.geom, s2.geom)
 ORDER BY s1."RoadName";
-
-ALTER TABLE ONLY mhtc_operations."Supply_Overlaps"
-    ADD CONSTRAINT "Supply_Overlaps_pkey" PRIMARY KEY ("GeometryID");
 
 ALTER TABLE mhtc_operations."Supply_Overlaps"
   OWNER TO postgres;
