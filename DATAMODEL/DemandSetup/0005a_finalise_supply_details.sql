3e94dd18acc8ae855ee7d04f1a3f9d48e52866d6
tlh22 <thancock@mhtc.co.uk>
2021-03-25 17:02:58 +0100

    Update 0005a_finalise_supply_details.sql

--- DATAMODEL/DemandSetup/0005a_finalise_supply_details.sql
+++ DATAMODEL/DemandSetup/0005a_finalise_supply_details.sql
@@ -2,6 +2,7 @@
 ALTER TABLE mhtc_operations."Supply"
   ADD COLUMN "Capacity" integer;
 
+DROP TRIGGER IF EXISTS "update_capacity_supply" ON "mhtc_operations"."Supply";
 CREATE TRIGGER "update_capacity_supply" BEFORE INSERT OR UPDATE OF geom, "RestrictionLength", "NrBays" ON "mhtc_operations"."Supply" FOR EACH ROW EXECUTE FUNCTION "public"."update_capacity"();
 
 UPDATE "mhtc_operations"."Supply"
