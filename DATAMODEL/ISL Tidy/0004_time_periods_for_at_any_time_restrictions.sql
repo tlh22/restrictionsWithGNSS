70cb6d2bbf4a0132e228f526e50d415c200b21e4
tlh22 <thancock@mhtc.co.uk>
2021-03-30 09:18:27 +0200

    Update 0004_time_periods_for_at_any_time_restrictions.sql

--- DATAMODEL/ISL Tidy/0004_time_periods_for_at_any_time_restrictions.sql
+++ DATAMODEL/ISL Tidy/0004_time_periods_for_at_any_time_restrictions.sql
@@ -14,6 +14,10 @@ SET "MatchDayTimePeriodID" = NULL
 WHERE "NoWaitingTimeID" = 1
 AND "MatchDayTimePeriodID" IS NOT NULL;
 
+UPDATE toms."Lines"
+SET "NoWaitingTimeID" = 1
+WHERE "NoWaitingTimeID" IS NULL;
+
 ALTER TABLE toms."Bays" ENABLE TRIGGER all;
 ALTER TABLE toms."Lines" ENABLE TRIGGER all;
 
