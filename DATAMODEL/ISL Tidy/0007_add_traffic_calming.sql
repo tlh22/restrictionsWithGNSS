a36aca42066871f715389f29d6c776f7903289cd
tlh22 <thancock@mhtc.co.uk>
2021-04-12 10:54:43 +0200

    Highway assets cont

--- DATAMODEL/ISL Tidy/0007_add_traffic_calming.sql
+++ DATAMODEL/ISL Tidy/0007_add_traffic_calming.sql
@@ -15,6 +15,8 @@ AND ST_Within(a.wkb_geometry, i.geom);
 
 -- check for OS records that were not picked up in the field
 
+DROP TABLE IF EXISTS mhtc_operations."TrafficCalming_NotFound" CASCADE;
+
 CREATE TABLE mhtc_operations."TrafficCalming_NotFound"
 AS
 SELECT p1."GeometryID", p1.geom_polygon
@@ -25,12 +27,17 @@ SELECT p2."GeometryID"
 FROM highway_assets."TrafficCalming" p2, highway_assets."TrafficCalming" l
 WHERE p2.geom_polygon IS NOT null
 AND l.geom IS NOT NULL
-AND ST_Intersects(ST_Buffer(l.geom, 0.5), p2.geom)
+AND ST_Intersects(ST_Buffer(l.geom, 0.5), p2.geom_polygon)
 );
+
+ALTER TABLE ONLY mhtc_operations."TrafficCalming_NotFound"
+    ADD CONSTRAINT "TrafficCalming_NotFound_pkey" PRIMARY KEY ("GeometryID");
 
 GRANT SELECT, UPDATE, INSERT, DELETE ON mhtc_operations."TrafficCalming_NotFound" TO toms_operator, toms_admin;
 
 -- check for features that do not appear in OS topo, but were picked up in the field
+
+DROP TABLE IF EXISTS mhtc_operations."TrafficCalming_Extras" CASCADE;
 
 CREATE TABLE mhtc_operations."TrafficCalming_Extras"
 AS
@@ -42,7 +49,36 @@ SELECT l."GeometryID"
 FROM highway_assets."TrafficCalming" p2, highway_assets."TrafficCalming" l
 WHERE p2.geom_polygon IS NOT null
 AND l.geom IS NOT NULL
-AND ST_Intersects(ST_Buffer(l.geom, 0.5), p2.geom)
+AND ST_Intersects(ST_Buffer(l.geom, 0.5), p2.geom_polygon)
 );
 
+ALTER TABLE ONLY mhtc_operations."TrafficCalming_Extras"
+    ADD CONSTRAINT "TrafficCalming_Extras_pkey" PRIMARY KEY ("GeometryID");
+
 GRANT SELECT, UPDATE, INSERT, DELETE ON mhtc_operations."TrafficCalming_Extras" TO toms_operator, toms_admin;
+
+-- Now get the details from the survey ...
+
+ALTER TABLE highway_assets."TrafficCalming" DISABLE TRIGGER all;
+
+UPDATE highway_assets."TrafficCalming" AS tc1
+SET "TrafficCalmingTypeID" = tc2."TrafficCalmingTypeID",
+    "NrCushions" = tc2."NrCushions",
+    "Photos_01"=tc2."Photos_01",
+    "Photos_02"=tc2."Photos_02",
+    "Photos_03"=tc2."Photos_03",
+    "LastUpdateDateTime"=tc2."LastUpdateDateTime",
+    "LastUpdatePerson"=tc2."LastUpdatePerson",
+    "MHTC_CheckIssueTypeID"=tc2."MHTC_CheckIssueTypeID",
+    "MHTC_CheckNotes"=tc2."MHTC_CheckNotes",
+    "FieldCheckCompleted"=tc2."FieldCheckCompleted",
+    "Last_MHTC_Check_UpdateDateTime"=tc2."Last_MHTC_Check_UpdateDateTime",
+    "Last_MHTC_Check_UpdatePerson"=tc2."Last_MHTC_Check_UpdatePerson",
+    "CreateDateTime"=tc2."CreateDateTime",
+    "CreatePerson"=tc2."CreatePerson"
+FROM highway_assets."TrafficCalming" AS tc2
+WHERE tc1.geom_polygon IS NOT NULL
+AND tc2.geom IS NOT NULL
+AND ST_Intersects(ST_Buffer(tc2.geom, 0.5), tc1.geom_polygon);
+
+ALTER TABLE highway_assets."TrafficCalming" ENABLE TRIGGER all;
