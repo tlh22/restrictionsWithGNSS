c68a991f33f78ffab42df26fc2b070876703de80
tlh22 <thancock@mhtc.co.uk>
2021-04-28 12:32:22 +0200

    plus

---
+++ DATAMODEL/DemandSetup/0000_remove_test_data.sql
@@ -0,0 +1,24 @@
+/**
+remove any test data
+**/
+
+DELETE
+FROM toms."Bays";
+
+DELETE
+FROM toms."Lines";
+
+DELETE
+FROM toms."Signs";
+
+DELETE
+FROM toms."RestrictionPolygons";
+
+DELETE
+FROM toms."ControlledParkingZones";
+
+DELETE
+FROM toms."ParkingTariffAreas";
+
+DELETE
+FROM toms."MatchDayEventDayZones";
