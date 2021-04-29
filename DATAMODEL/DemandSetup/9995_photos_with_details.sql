d5d2ed1204eb029ef884c0416c3a4e123c95e246
tlh22 <thancock@mhtc.co.uk>
2021-04-29 18:56:37 +0200

    Create 9995_photos_with_details.sql

---
+++ DATAMODEL/DemandSetup/9995_photos_with_details.sql
@@ -0,0 +1,34 @@
+/*
+add any relevant details
+Use this to create a .bat file; create the relevant destination folder and run from the source folder
+*/
+SELECT CONCAT('copy ', RiS."Photos_01", ' "../Photos_With_Details/', s."BeatTitle", '_', a."SectionName", '_', RiS."Photos_01", '"')
+FROM demand."RestrictionsInSurveys_ALL" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID") a
+WHERE "Photos_01" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
+
+UNION
+
+SELECT CONCAT('copy ', RiS."Photos_02", ' "../Photos_With_Details/', s."BeatTitle", '_', a."SectionName", '_', RiS."Photos_02", '"')
+FROM demand."RestrictionsInSurveys_ALL" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID") a
+WHERE "Photos_02" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
+
+UNION
+
+SELECT CONCAT('copy ', RiS."Photos_03", ' "../Photos_With_Details/', s."BeatTitle", '_', a."SectionName", '_', RiS."Photos_03", '"')
+FROM demand."RestrictionsInSurveys_ALL" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID") a
+WHERE "Photos_03" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
