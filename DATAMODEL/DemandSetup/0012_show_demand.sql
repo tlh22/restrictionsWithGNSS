ea3573a2c61ef44a373534614c1eb2d5f2ebe914
tlh22 <thancock@mhtc.co.uk>
2021-05-12 16:13:12 +0200

    show demand

--- DATAMODEL/DemandSetup/0012_show_demand.sql
+++ DATAMODEL/DemandSetup/0012_show_demand.sql
@@ -1,6 +1,6 @@
 -- create supply table that shows bay divisions
 
-DROP TABLE IF EXISTS demand."Supply_for_viewing_demand";
+DROP TABLE IF EXISTS demand."Supply_for_viewing_demand" CASCADE;
 
 CREATE TABLE demand."Supply_for_viewing_demand"
 (
@@ -49,6 +49,7 @@ DROP MATERIALIZED VIEW IF EXISTS demand."Supply_view_to_show_stress";
 CREATE MATERIALIZED VIEW demand."Supply_view_to_show_stress"
 TABLESPACE pg_default
 AS
+/** -- Using demand_Merged
     SELECT row_number() OVER (PARTITION BY true::boolean) AS id,
     s."GeometryID", s.geom, s."RestrictionTypeID", s."GeomShapeID", s."AzimuthToRoadCentreLine", s."BayOrientation", s."NrBays", s."Capacity",
     d."SurveyID", d."sbays" AS "BaysSuspended", d."Demand" AS "Demand",
@@ -77,14 +78,34 @@ AS
                 END
         END AS "Stress"
 
-	FROM mhtc_operations."Supply" s, demand."Demand_Merged" d
+	FROM demand."Supply_for_viewing_demand" s, demand."Demand_Merged" d
 	WHERE d."GeometryID" = s."GeometryID"
+
+	**/
+    SELECT row_number() OVER (PARTITION BY true::boolean) AS id,
+    s."GeometryID", s.geom, s."RestrictionTypeID", s."GeomShapeID", s."AzimuthToRoadCentreLine", s."BayOrientation", s."NrBays", d."Capacity",
+    d."SurveyID", d."Demand" AS "Demand",
+
+    /* What to do about suspensions */
+    CASE
+        WHEN d."Capacity"::float = 0 THEN
+            CASE
+                WHEN d."Demand"::float > 0.0 THEN 1.0
+                ELSE 0.0
+            END
+        ELSE
+            (d."Demand"::float / s."Capacity"::float) * 1.0
+        END AS "Stress"
+
+	FROM demand."Supply_for_viewing_demand" s, demand."Demand_Merged_Final" d
+	WHERE d."GeometryID" = s."GeometryID"
+
 WITH DATA;
 
 ALTER TABLE demand."Supply_view_to_show_stress"
     OWNER TO postgres;
 
-CREATE UNIQUE INDEX "idx_supply_view_to_show_demand_id"
+CREATE UNIQUE INDEX "idx_supply_view_to_show_stress_id"
     ON demand."Supply_view_to_show_stress" USING btree
     (id)
     TABLESPACE pg_default;
