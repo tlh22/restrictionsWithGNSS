39f0990975d00eceda9f92ba105fe2bb3461a6ca
tlh22 <thancock@mhtc.co.uk>
2021-05-13 21:34:37 +0200

    Update 0012_show_demand.sql

--- DATAMODEL/DemandSetup/0012_show_demand.sql
+++ DATAMODEL/DemandSetup/0012_show_demand.sql
@@ -1,6 +1,6 @@
 -- create supply table that shows bay divisions
 
-DROP TABLE IF EXISTS demand."Supply_for_viewing_demand";
+DROP TABLE IF EXISTS demand."Supply_for_viewing_demand" CASCADE;
 
 CREATE TABLE demand."Supply_for_viewing_demand"
 (
@@ -39,7 +39,7 @@ SELECT "GeometryID", geom, "RestrictionLength", "RestrictionTypeID",
              WHEN "GeomShapeID" >= 10 AND "GeomShapeID" < 20 THEN 21
              ELSE "GeomShapeID"
          END
-         , "AzimuthToRoadCentreLine", "BayOrientation", "Capacity", "Capacity"
+         , "AzimuthToRoadCentreLine", "BayOrientation", "Capacity"+1, "Capacity"
 	FROM mhtc_operations."Supply";
 
 -- create view with to show stress
@@ -51,33 +51,29 @@ TABLESPACE pg_default
 AS
     SELECT row_number() OVER (PARTITION BY true::boolean) AS id,
     s."GeometryID", s.geom, s."RestrictionTypeID", s."GeomShapeID", s."AzimuthToRoadCentreLine", s."BayOrientation", s."NrBays", s."Capacity",
-    d."SurveyID", d."sbays" AS "BaysSuspended", d."Demand" AS "Demand",
+    d."SurveyID", --d."sbays" AS "BaysSuspended",
+    d."Demand" AS "Demand",
 
     /* What to do about suspensions */
     CASE
-        WHEN s."Capacity" = 0 THEN
+        WHEN s."Capacity"::float = 0 THEN
             CASE
-                WHEN d."Demand" > 0.0 THEN 1.0
+                WHEN d."Demand"::float > 0.0 THEN 1.0
                 ELSE 0.0
                 END
         ELSE
             CASE
-                WHEN d."Done" IS TRUE THEN
-                    CASE
-                        WHEN s."Capacity"::float - COALESCE(NULLIF(d."sbays",'')::float, 0.0) > 0.0 THEN
-                            d."Demand" / (s."Capacity"::float - COALESCE(NULLIF(d."sbays",'')::float, 0.0)) * 1.0
-                        ELSE
-                            CASE
-                                WHEN d."Demand" > 0.0 THEN 1.0
-                                ELSE  0.0
-                                END
-                        END
+                WHEN s."Capacity"::float > 0.0 THEN
+                    d."Demand"::float / (s."Capacity"::float) * 1.0
                 ELSE
-                    0.0
+                    CASE
+                        WHEN d."Demand"::float > 0.0 THEN 1.0
+                        ELSE  0.0
+                        END
                 END
         END AS "Stress"
 
-	FROM mhtc_operations."Supply" s, demand."Demand_Merged" d
+	FROM demand."Supply_for_viewing_demand" s, demand."Demand_Merged_Final" d
 	WHERE d."GeometryID" = s."GeometryID"
 WITH DATA;
 
