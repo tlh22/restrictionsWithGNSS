ead509e0c024d4b2978a48f18d8c47a4f11e9fd8
tlh22 <thancock@mhtc.co.uk>
2021-05-17 17:46:51 +0200

    Tidy

--- DATAMODEL/DemandSetup/0012_show_parking_locations.sql
+++ DATAMODEL/DemandSetup/0012_show_parking_locations.sql
@@ -1,8 +1,12 @@
 -- create supply table that shows bay divisions
 
-DROP TABLE IF EXISTS demand."Supply_for_viewing_demand" CASCADE;
+-- create view with to show stress
 
-CREATE TABLE demand."Supply_for_viewing_demand"
+DROP MATERIALIZED VIEW IF EXISTS demand."Demand_view_to_show_parking_locations";
+
+DROP TABLE IF EXISTS demand."Supply_for_viewing_parking_locations" CASCADE;
+
+CREATE TABLE demand."Supply_for_viewing_parking_locations"
 (
     "GeometryID" character varying(12) COLLATE pg_catalog."default" NOT NULL,
     geom geometry(LineString,27700) NOT NULL,
@@ -13,26 +17,26 @@ CREATE TABLE demand."Supply_for_viewing_demand"
     "BayOrientation" double precision,
     "NrBays" integer NOT NULL DEFAULT '-1'::integer,
     "Capacity" integer,
-    CONSTRAINT "Supply_for_viewing_demand_pkey" UNIQUE ("GeometryID")
+    CONSTRAINT "Supply_for_viewing_parking_locations_pkey" UNIQUE ("GeometryID")
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE demand."Supply_for_viewing_demand"
+ALTER TABLE demand."Supply_for_viewing_parking_locations"
     OWNER to postgres;
 -- Index: sidx_Supply_geom
 
 -- DROP INDEX mhtc_operations."sidx_Supply_geom";
 
-CREATE INDEX "sidx_Supply_for_viewing_demand_geom"
-    ON demand."Supply_for_viewing_demand" USING gist
+CREATE INDEX "sidx_Supply_for_viewing_parking_locations_geom"
+    ON demand."Supply_for_viewing_parking_locations" USING gist
     (geom)
     TABLESPACE pg_default;
 
 
 -- populate
 
-INSERT INTO demand."Supply_for_viewing_demand"(
+INSERT INTO demand."Supply_for_viewing_parking_locations"(
 	"GeometryID", geom, "RestrictionLength", "RestrictionTypeID", "GeomShapeID", "AzimuthToRoadCentreLine", "BayOrientation", "NrBays", "Capacity")
 SELECT "GeometryID", geom, "RestrictionLength", "RestrictionTypeID",
         CASE WHEN "GeomShapeID" < 10 THEN "GeomShapeID" + 20
@@ -43,11 +47,7 @@ SELECT "GeometryID", geom, "RestrictionLength", "RestrictionTypeID",
 
 	FROM mhtc_operations."Supply";
 
--- create view with to show stress
-
-DROP MATERIALIZED VIEW IF EXISTS demand."Supply_view_to_show_stress";
-
-CREATE MATERIALIZED VIEW demand."Supply_view_to_show_stress"
+CREATE MATERIALIZED VIEW demand."Demand_view_to_show_parking_locations"
 TABLESPACE pg_default
 AS
 /** -- Using demand_Merged
@@ -56,7 +56,7 @@ AS
     d."SurveyID", --d."sbays" AS "BaysSuspended",
     d."Demand" AS "Demand",
 
-    /* What to do about suspensions */
+    --What to do about suspensions
     CASE
         WHEN s."Capacity"::float = 0 THEN
             CASE
@@ -75,36 +75,25 @@ AS
                 END
         END AS "Stress"
 
-	FROM demand."Supply_for_viewing_demand" s, demand."Demand_Merged_Final" d
+	FROM demand."Supply_for_viewing_parking_locations" s, demand."Demand_Merged_Final" d
 	WHERE d."GeometryID" = s."GeometryID"
 
 	**/
     SELECT row_number() OVER (PARTITION BY true::boolean) AS id,
     s."GeometryID", s.geom, s."RestrictionTypeID", s."GeomShapeID", s."AzimuthToRoadCentreLine", s."BayOrientation", s."NrBays", d."Capacity",
-    d."SurveyID", d."Demand" AS "Demand",
+    d."SurveyID", d."Demand" AS "Demand"
 
-    /* What to do about suspensions */
-    CASE
-        WHEN d."Capacity"::float = 0 THEN
-            CASE
-                WHEN d."Demand"::float > 0.0 THEN 1.0
-                ELSE 0.0
-            END
-        ELSE
-            (d."Demand"::float / s."Capacity"::float) * 1.0
-        END AS "Stress"
-
-	FROM demand."Supply_for_viewing_demand" s, demand."Demand_Merged_Final" d
+    FROM demand."Supply_for_viewing_parking_locations" s, demand."Demand_Merged_Final" d
 	WHERE d."GeometryID" = s."GeometryID"
 
 WITH DATA;
 
-ALTER TABLE demand."Supply_view_to_show_stress"
+ALTER TABLE demand."Demand_view_to_show_parking_locations"
     OWNER TO postgres;
 
-CREATE UNIQUE INDEX "idx_supply_view_to_show_stress_id"
-    ON demand."Supply_view_to_show_stress" USING btree
+CREATE UNIQUE INDEX "idx_Demand_view_to_show_parking_locations_id"
+    ON demand."Demand_view_to_show_parking_locations" USING btree
     (id)
     TABLESPACE pg_default;
 
-REFRESH MATERIALIZED VIEW demand."Supply_view_to_show_stress";
+REFRESH MATERIALIZED VIEW demand."Demand_view_to_show_parking_locations";
