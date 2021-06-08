5a97c155d2f2383423c7f388ffdd9e7bb2c98114
tlh22 <thancock@mhtc.co.uk>
2021-06-08 21:58:12 +0200

    Update 9004_copy_files.sh

--- DATAMODEL/HaveringCorners/9004_copy_files.sh
+++ DATAMODEL/HaveringCorners/9004_copy_files.sh
@@ -1,12 +1,33 @@
+/*
+ * script for copying files
+ */
+
+filename: organise_photos.sh
 #!/bin/bash
-file="junction_list.csv"
-while IFS=: read -r f1 f2
+file="$1"
+printf "filename: %s\n" "$file"
+#
+stripDoubleQuotes () {
+    var="$@"
+    # https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo "$var" | sed "s/\"//g"
+}
+while IFS="," read -r Geom_ID Ward
 do
-        # display fields using f1, f2,..,f7
-        printf 'GeometryID: %s, Ward: %s, Home Dir: %s\n' "$f1" "$f2"
-done <"$file"
+    #printf "GeometryID: %s; Ward: %s\n" "$Geom_ID" "$Ward"
+    #printf "GeometryID: %s; Ward: %s\n" "$(stripDoubleQuotes) $Geom_ID" $(stripDoubleQuotes) "$Ward"
+    #
+    geom_id="$(stripDoubleQuotes $Geom_ID)"
+    output_directory="$(pwd)"/"$(stripDoubleQuotes $Ward)"
+    #printf "file_name: %s folder: %s\n" "$geom_id" "$output_directory"
+    mkdir -p -v "$output_directory"
+    #
+    find /home/QGIS/projects/Havering/Mapping -name "*$geom_id*.png" -exec cp -v {} "$output_directory" \;
+done < "$file"
 
-find . -name \*.jar | xargs cp -t /destination_dir
 
-
-/home/tim/HVG_Photos/junction_list.csv
+Useage: $ sh organise_photos.sh <file_with_photo_list e.g., junction_list.csv>
