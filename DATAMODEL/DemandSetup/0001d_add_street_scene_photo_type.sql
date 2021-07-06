8ec9aef411d97b3777c3819253e654cf6e0e0fa7
tlh22 <thancock@mhtc.co.uk>
2021-07-06 09:34:26 +0200

    tidy3

--- DATAMODEL/DemandSetup/0001d_add_street_scene_photo_type.sql
+++ DATAMODEL/DemandSetup/0001d_add_street_scene_photo_type.sql
@@ -1,10 +1,25 @@
 /**
- Add street scene
-
+ Include only required sign types
 **/
 
-INSERT INTO toms_lookups."SignTypesInUse"(
-	"Code")
-	VALUES (9999);
+DELETE FROM toms_lookups."SignTypesInUse";
+
+INSERT INTO toms_lookups."SignTypesInUse"("Code")
+    SELECT "Code"
+	FROM toms_lookups."SignTypes"
+	WHERE "Description" LIKE ('Parking%')
+	OR "Description" LIKE ('Parking%')
+	OR "Code" IN (0, 37, 9999)
+	ORDER BY "Description";
 
 REFRESH MATERIALIZED VIEW "toms_lookups"."SignTypesInUse_View";
+
+/**
+Ensure appropriate bay/line types
+**/
+
+INSERT INTO toms_lookups."LineTypesInUse"(
+	"Code", "GeomShapeGroupType")
+	VALUES (225, 'LineString');  -- Unmarked kerbline
+
+REFRESH MATERIALIZED VIEW "toms_lookups"."LineTypesInUse_View";
