3d71e95301bc4b7f42cd0b00196f7d302c210510
tlh22 <thancock@mhtc.co.uk>
2021-08-08 09:45:49 +0200

    tidy unmarked areas

--- DATAMODEL/DemandSetup/0005c_simplify_unmarked_areas.sql
+++ DATAMODEL/DemandSetup/0005c_simplify_unmarked_areas.sql
@@ -12,13 +12,37 @@ WHERE "RestrictionTypeID" in (201, 221);
 
 -- or the other way
 
+--SYLs
+
 UPDATE mhtc_operations."Supply"
 SET "RestrictionTypeID" = 201
 WHERE "RestrictionTypeID" = 224
 AND "UnacceptableTypeID" IS NULL;
 
 UPDATE mhtc_operations."Supply"
+SET "RestrictionTypeID" = 221
+WHERE "RestrictionTypeID" = 224
+AND "UnacceptableTypeID" IS NOT NULL;
+
+-- Unmarked
+
+UPDATE mhtc_operations."Supply"
 SET "RestrictionTypeID" = 216
 WHERE "RestrictionTypeID" = 225
 AND "UnacceptableTypeID" IS NULL;
 
+UPDATE mhtc_operations."Supply"
+SET "RestrictionTypeID" = 220
+WHERE "RestrictionTypeID" = 225
+AND "UnacceptableTypeID" IS NOT NULL;
+
+-- Unmarked within PPZ
+UPDATE mhtc_operations."Supply"
+SET "RestrictionTypeID" = 227
+WHERE "RestrictionTypeID" = 229
+AND "UnacceptableTypeID" IS NULL;
+
+UPDATE mhtc_operations."Supply"
+SET "RestrictionTypeID" = 228
+WHERE "RestrictionTypeID" = 229
+AND "UnacceptableTypeID" IS NOT NULL;
