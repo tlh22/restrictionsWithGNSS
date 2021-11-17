a157e770ee0eb9b469f62b2c30353d0531dfba30
tlh22 <thancock@mhtc.co.uk>
2021-11-17 06:19:22 +0100

    Update 0022_photos_by_id.sql

--- DATAMODEL/DemandSetup/0022_photos_by_id.sql
+++ DATAMODEL/DemandSetup/0022_photos_by_id.sql
@@ -8,4 +8,16 @@ SELECT "GeometryID", "SectionID", "SurveyID", "Photos_01", "Photos_02", "Photos_
 	FROM demand."MASTER_Saturday_Merged"
 	WHERE "Photos_01" IS NOT NULL OR "Photos_02" IS NOT NULL OR "Photos_03" IS NOT NULL;
 
- SELECT mhtc_operations.getPhotosFromTable(demand."MASTER_Saturday_Merged")
+SELECT mhtc_operations.getPhotosFromTable(demand."MASTER_Saturday_Merged")
+
+
+SELECT CONCAT('copy ', RiS."Photos_01", ' "../Photos_With_Details/', a."SectionName", '_', s."BeatTitle", '_', RiS."Photos_01", '"')
+FROM demand."Demand_Merged" RiS, demand."Surveys" s,
+(SELECT su."GeometryID", r."SectionName"
+FROM mhtc_operations."RC_Sections_merged" r, mhtc_operations."Supply" su
+WHERE r."gid" = su."SectionID"
+--AND su."CPZ" = 'FPC'
+) a
+WHERE "Photos_01" IS NOT NULL
+AND RiS."SurveyID" = s."SurveyID"
+AND a."GeometryID" = RiS."GeometryID"
