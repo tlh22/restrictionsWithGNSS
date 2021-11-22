35ce17c4e26ddabbb278a501724e381a0a793141
tlh22 <thancock@mhtc.co.uk>
2021-11-22 21:28:05 +0100

    Red route and field project file

--- DATAMODEL/DemandSetup/0001d3_add_required_types_for_red_routes.sql
+++ DATAMODEL/DemandSetup/0001d3_add_required_types_for_red_routes.sql
@@ -2,23 +2,16 @@
  Include only required sign types
 **/
 
-DELETE FROM toms_lookups."SignTypesInUse";
-
 INSERT INTO toms_lookups."SignTypesInUse"("Code")
     SELECT "Code"
 	FROM toms_lookups."SignTypes"
-	WHERE "Description" LIKE ('Parking%')
-	OR "Description" LIKE ('Zone%')
-	OR "Code" IN (0, 37, 9999)
+	WHERE "Description" LIKE ('Parking - Red Route/Greenway%')
+	AND "Code" NOT IN (SELECT "Code"
+	                   FROM toms_lookups."SignTypes"
+	                   WHERE "Description" LIKE ('Parking - Red Route/Greenway%')
+	                   AND "Code" NOT IN (SELECT "Code" FROM toms_lookups."SignTypesInUse")
+	                   )
 	ORDER BY "Description";
-
-/*
-INSERT INTO toms_lookups."SignTypesInUse"("Code")
-    SELECT "Code"
-	FROM toms_lookups."SignTypes"
-	WHERE "Code" IN (43)
-	ORDER BY "Description";
-*/
 
 REFRESH MATERIALIZED VIEW "toms_lookups"."SignTypesInUse_View";
 
@@ -73,7 +66,7 @@ add any time periods required
 INSERT INTO toms_lookups."TimePeriodsInUse"("Code")
 SELECT "Code"
 FROM toms_lookups."TimePeriods"
-WHERE "Code" IN (253, 370, 496, 617, 618);
+WHERE "Code" IN (8, 253, 618, 619);
 
 REFRESH MATERIALIZED VIEW "toms_lookups"."TimePeriodsInUse_View";
 */
