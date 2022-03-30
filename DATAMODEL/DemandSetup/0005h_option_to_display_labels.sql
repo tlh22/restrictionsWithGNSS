6b418e099de0e1be553d35a0f4dc5ffe8f889fe1
tlh22 <thancock@mhtc.co.uk>
2022-03-30 12:18:30 +0200

    refinements

--- DATAMODEL/DemandSetup/0005h_option_to_display_labels.sql
+++ DATAMODEL/DemandSetup/0005h_option_to_display_labels.sql
@@ -5,8 +5,3 @@ Adds field to allow turn on/off of label. This avoid multiple labels/leaders bei
 ALTER TABLE "mhtc_operations"."Supply"
     ADD COLUMN "DisplayLabel" boolean DEFAULT TRUE NOT NULL;
 
-ALTER TABLE "mhtc_operations"."Supply"
-    ADD COLUMN "label_Rotation" double precision;
-
-ALTER TABLE "toms"."RestrictionPolygons"
-    ADD COLUMN "DisplayLabel" boolean DEFAULT TRUE NOT NULL;
