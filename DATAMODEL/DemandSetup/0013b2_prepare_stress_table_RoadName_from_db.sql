0343af029bf5cd6e3cad8c4fc2b6ab51c2c0503a
tlh22 <thancock@mhtc.co.uk>
2022-04-06 22:35:47 +0200

    stress from vrms

--- DATAMODEL/DemandSetup/0013b2_prepare_stress_table_RoadName_from_db.sql
+++ DATAMODEL/DemandSetup/0013b2_prepare_stress_table_RoadName_from_db.sql
@@ -125,3 +125,60 @@ SELECT "SurveyID", "RoadName", "Capacity", "Demand",
     GROUP BY d."SurveyID", s."RoadName"
     ORDER BY s."RoadName", d."SurveyID" ) a
 
+
+-- Stress from vrms_final
+
+DROP MATERIALIZED VIEW IF EXISTS demand."StressResults";
+
+CREATE MATERIALIZED VIEW demand."StressResults"
+TABLESPACE pg_default
+AS
+    SELECT
+        row_number() OVER (PARTITION BY true::boolean) AS sid,
+    s."name1" AS "RoadName", s.geom,
+    d."SurveyID", d."Stress" AS "Stress"
+	FROM highways_network."roadlink" s,
+	(
+	SELECT "SurveyID", "RoadName",
+        CASE
+            WHEN "Capacity" = 0 THEN
+                CASE
+                    WHEN "Demand" > 0.0 THEN 1.0
+                    ELSE -1.0
+                END
+            ELSE
+                CASE
+                    WHEN "Capacity"::float > 0.0 THEN
+                        "Demand" / ("Capacity"::float)
+                    ELSE
+                        CASE
+                            WHEN "Demand" > 0.0 THEN 1.0
+                            ELSE -1.0
+                        END
+                END
+        END "Stress"
+    FROM (
+    SELECT "SurveyID", s."RoadName", SUM(s."Capacity") AS "Capacity", SUM(demand."Demand") AS "Demand"
+    FROM mhtc_operations."Supply" s, (
+        SELECT a."SurveyID", a."GeometryID", SUM("VehicleTypes"."PCU") AS "Demand"
+        FROM (demand."VRMs_Final" AS a
+        LEFT JOIN "demand_lookups"."VehicleTypes" AS "VehicleTypes" ON a."VehicleTypeID" is not distinct from "VehicleTypes"."Code")
+        GROUP BY a."SurveyID", a."GeometryID"
+    ) demand
+    WHERE s."GeometryID" = demand."GeometryID"
+    AND s."RestrictionTypeID" NOT IN (117, 118)  -- Motorcycle bays
+    GROUP BY demand."SurveyID", s."RoadName"
+    ORDER BY s."RoadName", demand."SurveyID" ) a
+    ) d
+	WHERE s."name1" = d."RoadName"
+WITH DATA;
+
+ALTER TABLE demand."StressResults"
+    OWNER TO postgres;
+
+CREATE UNIQUE INDEX "idx_StressResults_sid"
+    ON demand."StressResults" USING btree
+    (sid)
+    TABLESPACE pg_default;
+
+REFRESH MATERIALIZED VIEW demand."StressResults";
