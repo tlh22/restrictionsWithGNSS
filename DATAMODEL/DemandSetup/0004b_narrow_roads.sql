50098c0b0ac8779b1d2a0555c143f4f89c397ab3
tlh22 <thancock@mhtc.co.uk>
2022-06-02 10:53:08 +0200

    Make sure not adjoining

--- DATAMODEL/DemandSetup/0004b_narrow_roads.sql
+++ DATAMODEL/DemandSetup/0004b_narrow_roads.sql
@@ -15,6 +15,7 @@ FROM
   (SELECT gid, "RoadName", geom from mhtc_operations."RC_Sections_merged") as line2
 WHERE
   st_Intersects(line1.geom, st_buffer(line2.geom, 4.7))
+  AND NOT ST_DWithin(line1.geom, line2.geom, 0.25) -- make sure that they are not joined
   AND line1.gid <> line2.gid
   AND line1."RoadName" = line2."RoadName";
 
@@ -36,6 +37,7 @@ FROM
   (SELECT gid, "RoadName", geom from mhtc_operations."RC_Sections_merged") as line2
 WHERE
   st_intersects(line1.geom, st_buffer(line2.geom, 6.5))
+  AND NOT ST_DWithin(line1.geom, line2.geom, 0.25) -- make sure that they are not joined
   AND line1.gid <> line2.gid
   AND line1."RoadName" = line2."RoadName";
 
@@ -57,6 +59,7 @@ FROM
   (SELECT gid, "RoadName", geom from mhtc_operations."RC_Sections_merged") as line2
 WHERE
   st_intersects(line1.geom, st_buffer(line2.geom, 9.8))
+  AND NOT ST_DWithin(line1.geom, line2.geom, 0.25) -- make sure that they are not joined
   AND line1.gid <> line2.gid
   AND line1."RoadName" = line2."RoadName";
 
