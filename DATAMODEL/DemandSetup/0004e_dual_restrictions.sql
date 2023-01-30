478b460c5854cf274b773b29fd939b10585d0a8f
tlh22 <thancock@mhtc.co.uk>
2023-01-30 12:33:37 +0100

    Update 0004e_dual_restrictions.sql

--- DATAMODEL/DemandSetup/0004e_dual_restrictions.sql
+++ DATAMODEL/DemandSetup/0004e_dual_restrictions.sql
@@ -21,9 +21,150 @@ ALTER TABLE mhtc_operations."DualRestrictions"
 
 -- Supply output
 
+-- For RBKC need to consider that Market Bays, taxi bays and cycle hangards should have lower status than SYL/DYL.
+
 INSERT INTO mhtc_operations."DualRestrictions" ("GeometryID", "LinkedTo")
 SELECT s1."GeometryID", s2."GeometryID"
 FROM mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
 WHERE ST_INTERSECTS(ST_LineSubstring (s1.geom, 0.1, 0.9), ST_Buffer(s2.geom, 0.1, 'endcap=flat'))
 AND s1."GeometryID" != s2."GeometryID"
-AND s1."RestrictionTypeID" > 200;
+AND s1."RestrictionTypeID" > 200
+AND s2."RestrictionTypeID" NOT IN (121, 147, 151, 164);
+
+-- Now consider specifically
+INSERT INTO mhtc_operations."DualRestrictions" ("GeometryID", "LinkedTo")
+SELECT s1."GeometryID", s2."GeometryID"
+FROM mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
+WHERE ST_INTERSECTS(ST_LineSubstring (s1.geom, 0.1, 0.9), ST_Buffer(s2.geom, 0.1, 'endcap=flat'))
+AND s1."GeometryID" != s2."GeometryID"
+AND s1."RestrictionTypeID" IN (121, 147, 151, 164)
+AND s2."RestrictionTypeID" IN (201, 221, 224, 202);
+
+-- Also need to consider School Keep Clears and SYLs/DYLs
+
+INSERT INTO mhtc_operations."DualRestrictions" ("GeometryID", "LinkedTo")
+SELECT s1."GeometryID", s2."GeometryID"
+FROM mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
+WHERE ST_INTERSECTS(ST_LineSubstring (s1.geom, 0.1, 0.9), ST_Buffer(s2.geom, 0.1, 'endcap=flat'))
+AND s1."GeometryID" != s2."GeometryID"
+AND s1."RestrictionTypeID" IN (203, 204, 205, 206, 207, 208)
+AND s2."RestrictionTypeID" IN (201, 221, 224, 202);
+
+-- Remove duplicates
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d1
+USING mhtc_operations."DualRestrictions" d2
+WHERE d1."GeometryID" = d2."GeometryID"
+AND d1."LinkedTo" = d2."LinkedTo"
+AND d1.id < d2.id;
+
+-- Add geometry column
+
+ALTER TABLE mhtc_operations."DualRestrictions"
+    ADD COLUMN "geom" geometry(LineString,27700);
+
+UPDATE mhtc_operations."DualRestrictions" AS d
+SET geom = s.geom
+FROM mhtc_operations."Supply" s
+WHERE d."GeometryID" = s."GeometryID";
+
+-- Remove any short restrictions from this list
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+WHERE ST_Length(d.geom) < 1.0;
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."LinkedTo" = s."GeometryID"
+AND ST_Length(s.geom) < 1.0;
+
+-- Remove any restrictions that no longer exist
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+WHERE "GeometryID" NOT IN
+    (SELECT "GeometryID" FROM mhtc_operations."Supply");
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+WHERE "LinkedTo" NOT IN
+    (SELECT "GeometryID" FROM mhtc_operations."Supply");
+
+-- Remove any DYLs
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."GeometryID" = s."GeometryID"
+AND s."RestrictionTypeID" = 202;
+
+-- Remove and SYLs/DYLs
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
+WHERE d."GeometryID" = s1."GeometryID"
+AND s1."GeometryID" IN (201, 221, 224)
+AND d."LinkedTo" = s2."GeometryID"
+AND s2."RestrictionTypeID" = 202;
+
+-- Remove and SYLs/SYLs
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
+WHERE d."GeometryID" = s1."GeometryID"
+AND s1."RestrictionTypeID" IN (201, 221, 224)
+AND d."LinkedTo" = s2."GeometryID"
+AND s2."RestrictionTypeID" IN (201, 221, 224);
+
+-- Remove any situations where the ZigZag is linked to
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."LinkedTo" = s."GeometryID"
+AND s."RestrictionTypeID" IN (203, 204, 205, 206, 207, 208);
+
+-- Remove DRLs
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."GeometryID" = s."GeometryID"
+AND s."RestrictionTypeID" = 218;
+
+-- Remove and SYLs/SYLs
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s1, mhtc_operations."Supply" s2
+WHERE d."GeometryID" = s1."GeometryID"
+AND s1."RestrictionTypeID" IN (217, 222, 226)
+AND d."LinkedTo" = s2."GeometryID"
+AND s2."RestrictionTypeID" IN (217, 222, 226);
+
+-- Remove crossings
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."GeometryID" = s."GeometryID"
+AND s."RestrictionTypeID" IN (209, 210, 211, 212, 213, 214, 215);
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."LinkedTo" = s."GeometryID"
+AND s."RestrictionTypeID" IN (209, 210, 211, 212, 213, 214, 215);
+
+-- Remove any Cycle Hire bays
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."GeometryID" = s."GeometryID"
+AND s."RestrictionTypeID" IN (116);
+
+DELETE
+FROM mhtc_operations."DualRestrictions" d
+USING mhtc_operations."Supply" s
+WHERE d."LinkedTo" = s."GeometryID"
+AND s."RestrictionTypeID" IN (116);
+
+
