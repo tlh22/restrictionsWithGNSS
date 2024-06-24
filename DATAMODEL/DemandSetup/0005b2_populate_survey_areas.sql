2ed387519466979aca5bb80a94a760b65f2376c2
tlh22 <thancock@mhtc.co.uk>
2024-06-24 10:26:27 +0100

    guernsey

--- DATAMODEL/DemandSetup/0005b2_populate_survey_areas.sql
+++ DATAMODEL/DemandSetup/0005b2_populate_survey_areas.sql
@@ -68,14 +68,49 @@ ALTER TABLE IF EXISTS mhtc_operations."Supply"
 alter table mhtc_operations."Supply" alter column "SurveyAreaID" TYPE INTEGER  USING ("SurveyAreaID"::integer) ;
 
  ***/
- 
- /***
-ALTER TABLE IF EXISTS mhtc_operations."Supply"
-      ADD COLUMN "SiteAreaID" INTEGER;
-	
-UPDATE "mhtc_operations"."Supply" AS s
-SET "SiteAreaID" = a."id"
-FROM local_authority."SiteArea" a
+
+/***
+
+--
+-- Calculate length of section within area
+
+UPDATE "mhtc_operations"."RC_Sections_merged" AS s
+SET "SurveyAreaID" = a."Code"
+FROM mhtc_operations."SurveyAreas" a
 WHERE ST_WITHIN (s.geom, a.geom);
 
+SELECT a."SurveyAreaName", SUM(s."SectionLength")
+FROM mhtc_operations."RC_Sections_merged" s, mhtc_operations."SurveyAreas" a
+WHERE ST_WITHIN (s.geom, a.geom)
+GROUP BY a."SurveyAreaName"
+ORDER BY a."SurveyAreaName";
+
+-- OR
+
+UPDATE "mhtc_operations"."Supply" AS s
+SET "SurveyAreaID" = NULL;
+
+UPDATE "mhtc_operations"."Supply" AS s
+SET "SurveyAreaID" = a."Code"
+FROM mhtc_operations."SurveyAreas" a
+WHERE ST_WITHIN (s.geom, a.geom);
+
+SELECT a."SurveyAreaName", SUM(s."RestrictionLength") AS "RestrictionLength", SUM("Capacity") AS "Total Capacity",
+SUM (CASE WHEN "RestrictionTypeID" > 200 THEN 0 ELSE s."Capacity" END) AS "Bay Capacity"
+FROM mhtc_operations."Supply" s, mhtc_operations."SurveyAreas" a
+WHERE a."Code" = s."SurveyAreaID"
+--AND a."SurveyAreaName" LIKE 'V%'
+GROUP BY a."SurveyAreaName"
+ORDER BY a."SurveyAreaName";
 ***/
+-- CPZs
+/***
+SELECT a."CPZ", SUM(s."RestrictionLength") AS "RestrictionLength", SUM("Capacity") AS "Total Capacity",
+SUM (CASE WHEN s."RestrictionTypeID" > 200 THEN 0 ELSE s."Capacity" END) AS "Bay Capacity"
+FROM mhtc_operations."Supply" s, toms."ControlledParkingZones" a
+WHERE ST_WITHIN (s.geom, a.geom)
+--AND a."SurveyAreaName" LIKE 'V%'
+GROUP BY a."CPZ"
+ORDER BY a."CPZ";
+***/
+
