dbc0559ea21d14fd0f5cabec6b4f488118facbac
tlh22 <thancock@mhtc.co.uk>
2025-04-17 11:06:32 +0200

    Tidy

--- DATAMODEL/DemandSetup/0001h_check_road_lengths.sql
+++ DATAMODEL/DemandSetup/0001h_check_road_lengths.sql
@@ -4,7 +4,8 @@ Check lengths of road
 
 ***/
 
-SELECT name1, SUM(ST_Length(geom))
+SELECT name1, SUM(ST_Length(geom)) AS "Length"
 FROM highways_network."roadlink"
 GROUP BY name1
+ORDER BY "Length" DESC
 
